26145cff936a1f5ac66d80e425d2c3dcee4c1390
malcondev.lds@hotmail.com <malcondev.lds@hotmail.com>
2026-08-22 00:33:42 -0300

    nlp

--- src/main.py
+++ src/main.py
@@ -26,4 +26,4 @@ if __name__ == '__main__':
         app.startup()
 
     except KeyboardInterrupt:
-        print("finalizando...")
+        print("\nFinalizando...")
---
+++ src/nlp/normalizer.py
@@ -0,0 +1,35 @@
+import unicodedata
+
+
+def text_lower(text):
+    """Transforma o texto de entrada todo em minúsculo"""
+
+    return text.lower()
+
+
+def whitespace(text):
+    """Remove espaços em branco"""
+
+    return " ".join(text.split())
+
+
+def remove_accents(text):
+    """Remove acentos como ex: ~^"""
+
+    no_accents = unicodedata.normalize("NFD", text)
+    no_accents = "".join(
+        char for char in no_accents if unicodedata.category(char) != "Mn"
+    )
+
+    return no_accents
+
+
+def normalize(text):
+    """Normaliza o texto para melhor tratamento"""
+
+    normalized = text_lower(text)
+    normalized = whitespace(normalized)
+    normalized = remove_accents(normalized)
+
+    return normalized
+
