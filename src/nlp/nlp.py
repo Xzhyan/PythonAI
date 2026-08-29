87b565b9ed3ddbd50e96e583a9211dd7fcf9eecb
malcondev.lds@hotmail.com <malcondev.lds@hotmail.com>
2026-08-29 09:32:01 -0300

    mudanças no vocabulary e embedding agora ta indo certo

--- src/nlp/nlp.py
+++ src/nlp/nlp.py
@@ -3,12 +3,16 @@ from .tokenizer import tokenize
 
 
 from .vocabulary import Vocabulary
-# from .embedding import Embedding
+from .embedding import Embedding
 
 
 class NLProcessing:
     def __init__(self):
-        self.vocab = Vocabulary(load=True)
+        self.vocab = Vocabulary(load=True, save=False)
+
+        vocab_size = self.vocab.token_count
+        dimension = 5
+        self.embedding = Embedding(vocab_size, dimension, load=True)
 
     def manage(self, entries):
         """Recebe as entradas e faz o tratamento"""
@@ -16,68 +20,4 @@ class NLProcessing:
         normalized = normalize(entries)
         tokens = tokenize(normalized)
 
-        print(self.vocab.id_to_token)
-
-
-
-# class NLPControl:
-#     def __init__(self):
-#         self.vocab = Vocabulary()
-#         self.embedding = Embedding(16, 3)
-
-
-#     def load_embedding(self):
-#         """Carrega o arquivo embedding.npy de matrizes"""
-
-#         # self.embedding.load_matriz()
-
-#         return self.embedding.weights
-
-#     def save_embedding(self):
-#         """Salva a matriz de todos os tokens do vocabulary"""
-
-#         id_list = []
-
-#         for id in self.vocab.id_to_token:
-#             id_list.append(id)
-
-#         self.embedding.get_vector(id_list)
-#         self.embedding.save_matriz()
-
-#     def control(self):
-#         # self.save_embedding()
-
-#         test = self.load_embedding()
-
-#         print(test)
-
-
-# def nlp_control(entries):
-#     """Recebe a entrada do usuário e processa"""
-
-#     normalized = normalize(entries)
-#     tokens = tokenize(normalized)
-
-#     # Trata os tokens no Vocabulary
-#     vocab.add_tokens(tokens)
-
-#     id_list = [] # lista de id dos tokens
-
-#     # Para cada token ele obtem o IDs persistentes no vocabulary
-#     for token in tokens:
-#         id = vocab.token_to_id[token]
-#         id_list.append(id)
-
-
-#     # obtem o vector apartir dos IDs na lista
-#     # vector = embedding.get_vector(id_list)
-
-#     # Carrega os pesos dos embeddins salvos
-#     # embedding.load_matriz()
-
-#     # print('salvando a matriz')
-#     # embedding.save_matriz()
-
-#     # print(embedding.weights)
-
-#     return tokens
+        print(self.embedding.weights)
--- src/nlp/vocabulary.py
+++ src/nlp/vocabulary.py
@@ -8,13 +8,15 @@ from utils.functions import write_json, read_json
 
 
 class Vocabulary:
-    def __init__(self, load=False):
+    def __init__(self, load=False, save=False):
         self.token_to_id = {}
         self.id_to_token = {}
 
         self.token_count = 0 # contagem de tokens do vocabulary
 
         self.updated = False # status de atualização do vocabulary
+
+        self.save = save # controle de salvamento do vocabulary.json
 
         # Se a instancia tiver load=True ele já carrega o vocabulary.json
         if load:
@@ -39,7 +41,7 @@ class Vocabulary:
             self.add_token_id(token)
 
         # verifica se tokens novos foram adicionados e atualiza o vocabulary
-        if self.updated:
+        if self.updated and self.save:
             self.save_vocab()
 
     def save_vocab(self):
@@ -52,84 +54,10 @@ class Vocabulary:
 
         data = read_json(VOCAB_JSON)
 
-        for token, id in data.items():
-            self.token_to_id[token] = token
-            self.id_to_token[id] = id
+        for token, token_id in data.items():
+
+            self.token_to_id[token] = token_id
+            self.id_to_token[token_id] = token
 
         self.token_count = len(self.token_to_id)
-
-
-
-# import json
-
-# # core
-# from core.config import VOCAB_JSON
-
-# # utils
-# from utils.functions import read_json, write_json
-
-
-# class Vocabulary:
-#     def __init__(self):
-#         self.token_to_id = {}
-#         self.id_to_token = {}
-
-#         # Logo que inicia já recupera o vocabulário do json
-#         self.load_vocabulary()
-
-#         # Identificar novos tokens e salvar .json
-#         self.update = False
-
-
-#     def add_token_id(self, token):
-#         """Adicionar id ao token"""
-
-#         if not token in self.token_to_id:
-#             token_id = len(self.token_to_id)
-
-#             self.token_to_id[token] = token_id
-#             self.id_to_token[token_id] = token
-
-#             self.update = True
-
-#         return self.token_to_id[token]
-
-
-#     def add_tokens(self, tokens):
-#         """Adiciona id a vários tokens"""
-
-#         for token in tokens:
-#             self.add_token_id(token)
-
-#         # Se novos tokens forem adicionados o vocabulary.json é salvo e recarregado
-#         if self.update:
-#             self.save_vocabulary()
-
-#             print("Atualizando o vocabulary.json")
-#             self.load_vocabulary()
-
-#             self.update = False # Depois de atualizar volta o valor de update para Falso
-
-
-#     def save_vocabulary(self):
-#         """Salva todos os dados do vocabulário no json"""
-
-#         print("\nSalvando o vocabulário")
-
-#         write_json(VOCAB_JSON, self.token_to_id)
-
-
-#     def load_vocabulary(self):
-#         """Carrega todo o vocabulário do arquivo .json"""
-
-#         data = read_json(VOCAB_JSON)
-
-#         for token in data.items():
-#             # cada token presente no 'data' do json já é salvo em token_to_id/id_to_token
-
-#             self.token_to_id[token[0]] = token[1]
-#             self.id_to_token[token[1]] = token[0]
-
-#         # contagem de tokens
-#         self.token_count = len(self.token_to_id)
 
